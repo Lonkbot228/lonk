1fff6b64661a5fea945ce003f0b8d8ece8ed83bf
Lonkbot228 <66420192+Lonkbot228@users.noreply.github.com>
2024-11-13 08:15:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ import re
 # Настройка Selenium
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-software-rasterizer')
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # Функция для получения расписания
